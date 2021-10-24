543f849b9cc8c21d66c31d22f9e95b8d62cab97d
Piotr Szulc <szulo123@gmail.com>
2021-10-24 10:03:28 +0200

    Update sensor last contact on update

--- SmartHomeCore/Domain/Sensor.cs
+++ SmartHomeCore/Domain/Sensor.cs
@@ -1,14 +1,17 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace SmartHomeCore.Domain
 {
     public class Sensor
     {
+        [Key]
         public Guid Id { get; init; }
+        [MaxLength(17)]
         public string Mac { get; init; }
-        public string Alias { get; init; }
-        public string ChipType { get; init; }
-        public DateTime? LastContact { get; init; }
-        public string FirmwareVersion { get; init; }
+        public string Alias { get; set; }
+        public string ChipType { get; set; }
+        public DateTime? LastContact { get; set; }
+        public string FirmwareVersion { get; set; }
     }
 }
--- SmartHomeWWW/Controllers/UpdateController.cs
+++ SmartHomeWWW/Controllers/UpdateController.cs
@@ -1,24 +1,30 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using SmartHomeCore.Domain;
 using SmartHomeCore.Firmwares;
+using SmartHomeCore.Infrastructure;
 using SmartHomeWWW.Models;
 using System;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace SmartHomeWWW.Controllers
 {
     public class UpdateController : Controller
     {
-        public UpdateController(ILogger<UpdateController> logger, IFirmwareRepository firmwareRepository)
+        public UpdateController(ILogger<UpdateController> logger, IFirmwareRepository firmwareRepository, SmartHomeDbContext dbContext)
         {
             _logger = logger;
             _firmwareRepository = firmwareRepository;
+            _dbContext = dbContext;
         }
 
         private readonly ILogger<UpdateController> _logger;
         private readonly IFirmwareRepository _firmwareRepository;
+        private readonly SmartHomeDbContext _dbContext;
 
         public IActionResult Index()
         {
@@ -32,7 +38,7 @@ namespace SmartHomeWWW.Controllers
         }
 
         [HttpGet("/{controller}/firmware.bin")]
-        public IActionResult Firmware()
+        public async Task<IActionResult> Firmware()
         {
             _logger.LogInformation(DumpHeaders(Request.Headers));
 
@@ -43,23 +49,49 @@ namespace SmartHomeWWW.Controllers
                 return new RedirectToActionResult("Index", "Update", null);
             }
 
-            var mac = Request.Headers["x-ESP8266-STA-MAC"];
+            var mac = Request.Headers["x-ESP8266-STA-MAC"].Single().ToUpper();
             _logger.LogInformation($"ESP8266 [{mac}] connected");
 
-            var deviceVersion = Request.Headers["x-ESP8266-version"];
+            var deviceVersion = Request.Headers["x-ESP8266-version"].Single();
+
+            await UpdateSensorInfo(mac, deviceVersion);
+
             if (!_firmwareRepository.TryGetCurrentVersion(out var currentVeresion))
             {
                 _logger.LogWarning($"No current version found");
                 return new StatusCodeResult(304);
             }
 
-            if (deviceVersion == currentVeresion)
+            if (deviceVersion == currentVeresion.ToString())
             {
                 _logger.LogInformation($"ESP8266 [{mac}] nothing new");
                 return new StatusCodeResult(304);
             }
 
             return new FileStreamResult(_firmwareRepository.GetCurrentFirmware(), "application/octet-stream");
+        }
+
+        private async Task UpdateSensorInfo(string mac, string firmwareVersion)
+        {
+            var sensor = await _dbContext.Sensors
+                .FirstOrDefaultAsync(s => s.Mac == mac);
+
+            if (sensor is null)
+            {
+                sensor = new Sensor
+                {
+                    Id = Guid.NewGuid(),
+                    Mac = mac,
+                };
+
+                _dbContext.Sensors.Add(sensor);
+            }
+
+            sensor.ChipType = "ESP8266";
+            sensor.LastContact = DateTime.UtcNow;
+            sensor.FirmwareVersion = firmwareVersion;
+
+            await _dbContext.SaveChangesAsync();
         }
 
         private static string DumpHeaders(IHeaderDictionary headers)
