745f294fa9c5b97ea4ac2c104681901f933c65bf
Piotr Szulc <szulo123@gmail.com>
2021-11-03 23:05:46 +0100

    Removed unnecessary usings

--- SmartHomeWWW/Startup.cs
+++ SmartHomeWWW/Startup.cs
@@ -1,5 +1,4 @@
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -7,10 +6,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using SmartHomeCore.Infrastructure;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace SmartHomeWWW
 {
