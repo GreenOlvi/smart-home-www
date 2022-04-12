cee3563cd5198e108a433733adfd447a592389f8
Piotr Szulc <szulo123@gmail.com>
2022-04-12 15:59:47 +0200

    Added TelegramUsers table

---
+++ Core/Domain/Entities/TelegramUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SmartHomeWWW.Core.Domain.Entities
+{
+    [Table("TelegramUsers")]
+    public class TelegramUser
+    {
+        [Key]
+        public Guid Id { get; init; }
+        public long TelegramId { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string UserType { get; set; } = string.Empty;
+    }
+}
---
+++ Server/Migrations/20220412140405_AddTelegramUsersTable.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartHomeWWW.Migrations
+{
+    public partial class AddTelegramUsersTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TelegramUsers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    TelegramId = table.Column<long>(type: "INTEGER", nullable: false),
+                    Username = table.Column<string>(type: "TEXT", nullable: false),
+                    UserType = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TelegramUsers", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TelegramUsers");
+        }
+    }
+}
