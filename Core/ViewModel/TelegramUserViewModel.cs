73c388fe1624efbe849f2ce378b675c2ecea5e4b
Piotr Szulc <szulo123@gmail.com>
2022-04-12 16:59:00 +0200

    Added TelegramUser CRUD api

---
+++ Core/ViewModel/TelegramUserViewModel.cs
@@ -0,0 +1,23 @@
+﻿using SmartHomeWWW.Core.Domain.Entities;
+using System;
+
+namespace SmartHomeWWW.Core.ViewModel
+{
+    public record TelegramUserViewModel
+    {
+        public Guid? Id { get; init; }
+        public long? TelegramId { get; set; }
+        public string? Username { get; set; }
+        public string? UserType { get; set; }
+
+        public static TelegramUserViewModel From(TelegramUser user) => new()
+        {
+            Id = user.Id,
+            TelegramId = user.TelegramId,
+            Username = user.Username,
+            UserType = user.UserType,
+        };
+
+        public static implicit operator TelegramUserViewModel(TelegramUser user) => From(user);
+    }
+}
