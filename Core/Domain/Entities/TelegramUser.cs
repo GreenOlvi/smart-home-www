6d615ae904d67364b26f68e0be315cc552e4219d
Piotr Szulc <szulo123@gmail.com>
2022-04-21 22:58:26 +0200

    Added index on telegram id

--- Core/Domain/Entities/TelegramUser.cs
+++ Core/Domain/Entities/TelegramUser.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SmartHomeWWW.Core.Domain.Entities
 {
     [Table("TelegramUsers")]
+    [Index(nameof(TelegramId), IsUnique = true)]
     public class TelegramUser
     {
         [Key]
---
+++ Server/Migrations/20220421205639_AddIndexToTelegramUsersOnTelegramId.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartHomeWWW.Migrations
+{
+    public partial class AddIndexToTelegramUsersOnTelegramId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_TelegramUsers_TelegramId",
+                table: "TelegramUsers",
+                column: "TelegramId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_TelegramUsers_TelegramId",
+                table: "TelegramUsers");
+        }
+    }
+}
