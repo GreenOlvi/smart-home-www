b313a9799481a5d5ac0cbd4f2e9cbffb0fc6e1ba
Piotr Szulc <szulo123@gmail.com>
2022-05-12 21:49:14 +0200

    Added weather page

---
+++ Client/HttpClients/WeatherHttpClient.cs
@@ -0,0 +1,19 @@
+﻿using SmartHomeWWW.Core.Domain.OpenWeatherMaps;
+using System.Net.Http.Json;
+
+namespace SmartHomeWWW.Client.HttpClients
+{
+    public class WeatherHttpClient
+    {
+        public WeatherHttpClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        private readonly HttpClient _httpClient;
+
+        public async Task<WeatherReport?> GetCurrent() =>
+            await _httpClient.GetFromJsonAsync<WeatherReport>("api/weather/current");
+
+    }
+}
