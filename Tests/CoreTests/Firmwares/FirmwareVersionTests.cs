4a19fbaf360222d1b4ac3636823553fffef044b2
Piotr Szulc <szulo123@gmail.com>
2022-10-19 23:05:44 +0200

    Compare device firmware on update

--- Tests/CoreTests/Firmwares/FirmwareVersionTests.cs
+++ Tests/CoreTests/Firmwares/FirmwareVersionTests.cs
@@ -20,4 +20,23 @@ public class FirmwareVersionTests
         result.Prefix.Should().Be(new Version(expectedPrefix));
         result.Suffix.Should().Be(expectedSuffix);
     }
+
+    [TestCase("0.0.4", "0.0.4", null)]
+    [TestCase("1.0.0-alpha", "1.0.0", "alpha")]
+    [TestCase("1.a-nonsense", null, null)]
+    public void TryParseTests(string input, string? expectedPrefix, string? expectedSuffix)
+    {
+        var success = FirmwareVersion.TryParse(input, out var result);
+
+        if (expectedPrefix is not null)
+        {
+            success.Should().BeTrue();
+            result.Prefix.Should().Be(new Version(expectedPrefix));
+            result.Suffix.Should().Be(expectedSuffix);
+        }
+        else
+        {
+            success.Should().BeFalse();
+        }
+    }
 }
