790e3dd126431b43bc2b73c3027de3eef6dd3c98
Piotr Szulc <szulo123@gmail.com>
2023-10-08 11:31:24 +0200

    Set DB PRAGMA journal to WAL

--- Server/Migrations/20231008092138_PragmaJournalToWal.Designer.cs
+++ Server/Migrations/20231008092138_PragmaJournalToWal.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SmartHomeWWW.Core.Infrastructure;
 
@@ -10,9 +11,11 @@ using SmartHomeWWW.Core.Infrastructure;
 namespace SmartHomeWWW.Migrations
 {
     [DbContext(typeof(SmartHomeDbContext))]
-    partial class SmartHomeDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231008092138_PragmaJournalToWal")]
+    partial class PragmaJournalToWal
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.11");
---
+++ Server/Migrations/20231008092138_PragmaJournalToWal.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartHomeWWW.Migrations
+{
+    /// <inheritdoc />
+    public partial class PragmaJournalToWal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("PRAGMA journal_mode = 'wal'", true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("PRAGMA journal_mode = 'delete'", true);
+        }
+    }
+}
